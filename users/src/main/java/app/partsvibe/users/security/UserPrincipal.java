d0b92c57d348bf21bc9e31672707b47af2bd5f15
Marcin Chwedczuk <0xmarcin+gh@gmail.com>
2026-01-31 18:07:58 +0100

    Store http session in database

--- users/src/main/java/app/partsvibe/users/security/UserPrincipal.java
+++ users/src/main/java/app/partsvibe/users/security/UserPrincipal.java
@@ -1,34 +1,45 @@
 package app.partsvibe.users.security;
 
+import app.partsvibe.users.domain.Role;
 import app.partsvibe.users.domain.UserAccount;
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.Collection;
+import java.util.Set;
 import java.util.stream.Collectors;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-public class UserPrincipal implements UserDetails {
-    private final UserAccount user;
+public class UserPrincipal implements UserDetails, Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private final String username;
+    private final String passwordHash;
+    private final boolean enabled;
+    private final Set<String> roleNames;
 
     public UserPrincipal(UserAccount user) {
-        this.user = user;
+        this.username = user.getUsername();
+        this.passwordHash = user.getPasswordHash();
+        this.enabled = user.isEnabled();
+        this.roleNames = user.getRoles().stream().map(Role::getName).collect(Collectors.toUnmodifiableSet());
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return user.getRoles().stream()
-                .map(role -> new SimpleGrantedAuthority(role.getName()))
-                .collect(Collectors.toSet());
+        return roleNames.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toSet());
     }
 
     @Override
     public String getPassword() {
-        return user.getPasswordHash();
+        return passwordHash;
     }
 
     @Override
     public String getUsername() {
-        return user.getUsername();
+        return username;
     }
 
     @Override
@@ -48,6 +59,6 @@ public class UserPrincipal implements UserDetails {
 
     @Override
     public boolean isEnabled() {
-        return user.isEnabled();
+        return enabled;
     }
 }
