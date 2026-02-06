7eda1b7e2376b970116f1d9dc197417ee5f56df8
Marcin Chwedczuk <0xmarcin+gh@gmail.com>
2026-02-06 16:20:47 +0100

    use lombok

--- catalog/src/main/java/app/partsvibe/catalog/web/CatalogIndexForm.java
+++ catalog/src/main/java/app/partsvibe/catalog/web/CatalogIndexForm.java
@@ -2,17 +2,13 @@ package app.partsvibe.catalog.web;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class CatalogIndexForm {
     @NotBlank
     @Size(max = 50000)
     private String text;
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
 }
--- site/src/main/java/app/partsvibe/site/web/ContactForm.java
+++ site/src/main/java/app/partsvibe/site/web/ContactForm.java
@@ -3,7 +3,11 @@ package app.partsvibe.site.web;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class ContactForm {
     @NotBlank
     @Size(max = 64)
@@ -15,28 +19,4 @@ public class ContactForm {
 
     @NotBlank
     private String message;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
