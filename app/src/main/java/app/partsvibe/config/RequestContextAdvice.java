6bff3bd8ac67b5d35ce65f81e6304a58eca7d93d
Marcin Chwedczuk <0xmarcin+gh@gmail.com>
2026-02-13 18:15:31 +0100

    ui bugfixes

--- app/src/main/java/app/partsvibe/config/RequestContextAdvice.java
+++ app/src/main/java/app/partsvibe/config/RequestContextAdvice.java
@@ -1,6 +1,9 @@
 package app.partsvibe.config;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
@@ -24,5 +27,13 @@ public class RequestContextAdvice {
     @ModelAttribute("appVersion")
     public String appVersion() {
         return buildVersionInfo.gitVersionLabel();
+    }
+
+    @ModelAttribute("isAuthenticatedUser")
+    public boolean isAuthenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication != null
+                && authentication.isAuthenticated()
+                && !(authentication instanceof AnonymousAuthenticationToken);
     }
 }
