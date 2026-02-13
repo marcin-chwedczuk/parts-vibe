2988c6a3047db48932ba6685e595875ae563e145
Marcin Chwedczuk <0xmarcin+gh@gmail.com>
2026-02-13 11:13:07 +0100

    add equals/hashCode to base entity

--- shared/src/main/java/app/partsvibe/shared/persistence/BaseEntity.java
+++ shared/src/main/java/app/partsvibe/shared/persistence/BaseEntity.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.Hibernate;
 
 @Getter
 @Setter
@@ -14,4 +15,21 @@ public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
+            return false;
+        }
+        BaseEntity that = (BaseEntity) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Hibernate.getClass(this).hashCode();
+    }
 }
