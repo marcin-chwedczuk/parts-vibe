79d8e8ece97479721d4a641b7a669d61e266044e
Marcin Chwedczuk <0xmarcin+gh@gmail.com>
2026-02-19 11:19:28 +0100

    convert FindUserDetailsByUsernameQueryHandler to queryDSL

--- users/src/main/java/app/partsvibe/users/queries/auth/FindUserDetailsByUsernameQueryHandler.java
+++ users/src/main/java/app/partsvibe/users/queries/auth/FindUserDetailsByUsernameQueryHandler.java
@@ -1,8 +1,10 @@
 package app.partsvibe.users.queries.auth;
 
 import app.partsvibe.shared.cqrs.BaseQueryHandler;
-import app.partsvibe.users.repo.UserRepository;
+import app.partsvibe.users.domain.QUser;
+import app.partsvibe.users.domain.User;
 import app.partsvibe.users.security.UserPrincipal;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,21 +15,26 @@ import org.springframework.stereotype.Component;
 class FindUserDetailsByUsernameQueryHandler extends BaseQueryHandler<FindUserDetailsByUsernameQuery, UserDetails> {
     private static final Logger log = LoggerFactory.getLogger(FindUserDetailsByUsernameQueryHandler.class);
 
-    private final UserRepository userRepository;
+    private final JPAQueryFactory queryFactory;
 
-    FindUserDetailsByUsernameQueryHandler(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    FindUserDetailsByUsernameQueryHandler(JPAQueryFactory queryFactory) {
+        this.queryFactory = queryFactory;
     }
 
     @Override
     protected UserDetails doHandle(FindUserDetailsByUsernameQuery query) {
+        QUser user = QUser.user;
         log.info("Loading user details for username={}", query.username());
-        return userRepository
-                .findByUsername(query.username())
-                .map(UserPrincipal::new)
-                .orElseThrow(() -> {
-                    log.warn("User not found: {}", query.username());
-                    return new UsernameNotFoundException("User not found: " + query.username());
-                });
+        User userDetails = queryFactory
+                .selectFrom(user)
+                .where(user.username.eq(query.username()))
+                .fetchOne();
+
+        if (userDetails == null) {
+            log.warn("User not found: {}", query.username());
+            throw new UsernameNotFoundException("User not found: " + query.username());
+        }
+
+        return new UserPrincipal(userDetails);
     }
 }
